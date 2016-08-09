d9bc9f6bdedd162604cac07570bce485ed2166e4
eric <eric.kong98@gmail.com>
2016-08-09 00:15:17 -0500

    changes to users class

--- application/models/users.py
+++ application/models/users.py
@@ -4,17 +4,19 @@ from sqlalchemy import Text
 from sqlalchemy import String
 from sqlalchemy import DateTime
 from sqlalchemy import Boolean
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "User"
+class Users(Base):
+    __tablename__ = "Users"
     id = Column(Integer, primary_key = True, autoincrement = True)
     username = Column(String(255))
     email = Column(String(255))
     password = Column(String(255))
-    premium = Column(Boolean, default = False)
+    premium = Column(Boolean, default = False, nullable = False)
     date_created = Column(DateTime)
+    deck_meta = relationship("DeckMeta", backref = "Users.id")
 
-    def __init__(self):
+    def __init__(self, username, email, password, date_created):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.date_created = date_created
