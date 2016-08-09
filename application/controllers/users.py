9117763f7eb881c5330fd05a9968d660fda739a2
eric <eric.kong98@gmail.com>
2016-08-09 01:53:49 -0400

    starte work on users controllers and fixing datetime

---
+++ application/controllers/users.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
+from sqlalchemy import create_engine
+from application import app
+from application import models
+
+def create_user(username, email, password):
+    engine = create_engine(app.config["DB"])
+    Session = sessionmaker(bind = engine)
+    session = Session()
+
+    hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    new_user = models.users.Users(username, email, hashed)
+
+    try:
+        session.add(new_user)
+        session.commit(new_user)
+    except OperationalError:
+        models.create_db(engine)
+        session.add(new_user)
+        session.commit(new_user)
+    session.close()
