01b0cfd69887269adae3fecb51ac064f512be7c4
eric <eric.kong98@gmail.com>
2016-08-09 23:00:51 -0400

    adding more controller functions for user model

--- application/controllers/users.py
+++ application/controllers/users.py
@@ -1,13 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import MultipleResultsFound
 from sqlalchemy import create_engine
 from application import app
 from application import models
+from . import engine
 
-def create_user(username, email, password):
-    engine = create_engine(app.config["DB"])
-    Session = sessionmaker(bind = engine)
-    session = Session()
+def create_user(username, email, password, session_object):
+    session = session_object()
 
     hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     new_user = models.users.Users(username, email, hashed)
@@ -19,4 +20,64 @@ def create_user(username, email, password):
         models.create_db(engine)
         session.add(new_user)
         session.commit()
-    session.close()
+    finally:
+        session.close()
+
+def get_user(id, session_object):
+    session = session_object()
+
+    def query_helper():
+        try:
+            user = session.query(models.users.Users).filter(models.users.Users.id == id).one()
+            if user is not None:
+                return user
+        except NoResultFound:
+            return None
+        except MultipleResultsFound:
+            return None
+
+    try:
+        return query_helper()
+    except OperationalError:
+        models.create_db(engine)
+        return query_helper()
+    finally:
+        session.close()
+
+def delete_user(id, session_object):
+    session = session_object()
+
+    user = get_user(id, session_object)
+    if user == None:
+        print("Error: User does not exist")
+        return
+    else:
+        try:
+            session.delete(user)
+            session.commit()
+        except OperationalError:
+            print("Error: Something is wrong")
+        finally:
+            session.close()
+
+def save_user(id, username, email, password, session_object):
+    session = session_object()
+    user = get_user(id, session_object)
+    if user == None:
+        print("Error: User does not exist")
+        return
+    hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    else:
+        try:
+            user.username = username
+            user.email = email
+            user.password = hashed
+            session.commit()
+        except OperationalError:
+            models.create_db(engine)
+            user.username = username
+            user.email = email
+            user.password = hashed
+            session.commit()
+        finally:
+            session.close()
